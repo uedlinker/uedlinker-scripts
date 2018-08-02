6d703358d745b371868e1a62075d3558128c1286
李蔚生 <971353677@qq.com>
2018-08-02 14:44:49 +0800

    feat: 支持用户自定义环境变量，名称需要以  开头

--- configs/webpack.common.js
+++ configs/webpack.common.js
@@ -1,5 +1,16 @@
 const path = require('path')
+const webpack = require('webpack')
 const { appPath, srcPath, entryPath, outputPath } = require('./paths')
+
+// 获取用户自定义环境变量并在 webpack 中定义，自定义环境变量必须以 `UEDLINKER_` 开头，
+// 在 `process.env` 对象上挂载了很多环境变量，但没有必须把这些环境变量都在 webpack 中定义。
+const customEnvs = {}
+const customEnvRe = /^UEDLINKER_/
+for (let key in process.env) {
+  if (customEnvRe.test(key)) {
+    customEnvs[`process.env.${key}`] = JSON.stringify(process.env[key])
+  }
+}
 
 module.exports = {
   context: appPath,
@@ -34,6 +45,10 @@ module.exports = {
     },
     runtimeChunk: true,
   },
+
+  plugins: [
+    new webpack.DefinePlugin(customEnvs),
+  ],
 
   node: {
     dgram: 'empty',
