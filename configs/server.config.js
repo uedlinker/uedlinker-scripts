6118c914961676ba7bc550fc34ada14d5f0ac9f2
RunningCoderLee <sprint_l@aliyun.com>
2018-08-24 18:28:51 +0800

    feat: 支持在启动时检测是否已有打开 chrome tab，如打开则不再开启新窗口
    close #1

--- configs/server.config.js
+++ configs/server.config.js
@@ -1,18 +1,35 @@
 const { staticPath } = require('./paths')
+const execSync = require('child_process').execSync;
 const webpackDevConfig = require('./webpack.dev')
 const combineConfig = require('../utils/combineConfig')
+
+const port = 3000
 
 const defaultServeConfig = {
   config: webpackDevConfig,
   content: staticPath,
-  open: true,
-  port: 3000,
+  open: false,
+  port: port,
   devMiddleware: {
     publicPath: webpackDevConfig.output.publicPath,
   },
   hotClient: {
     logLevel: 'warn',
   },
+  on: {
+    listening: () => {
+      execSync('ps cax | grep "Google Chrome"');
+      execSync(
+        `osascript chrome.applescript "${encodeURI(
+          `http://localhost:${port}`
+        )}"`,
+        {
+          cwd: __dirname,
+          stdio: 'ignore',
+        }
+      );
+    },
+  }
 }
 
 module.exports = combineConfig('server')(defaultServeConfig)
